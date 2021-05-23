bafead0466264b9a81fa7fc6b47899794856a876
leechangil <leechangil12@gmail.com>
2021-05-23 11:47:54 +0900

    Simple Chat

---
+++ Hubs/SimpleChat.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleChat.Hubs
+{
+    public class SimpleChat : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            Console.WriteLine(message);
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
